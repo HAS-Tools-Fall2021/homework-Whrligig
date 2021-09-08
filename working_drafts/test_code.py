c673c90f3da45a614fa8d1c06ac4d7c97767a0e6
Connal Boyd <cmboyd@email.arizona.edu>
2021-09-08 13:21:06 -0700

    Week 2 Python training test code

---
+++ working_drafts/test_code.py
@@ -0,0 +1,101 @@
+# %%
+
+## Variable Code Test
+
+my_variable = 5
+
+my_variable
+
+a = 3
+
+a
+
+## Variable Rename Test
+
+temperature = 55
+
+temperature
+
+temperature = 'doctor'
+
+temperature
+
+# %%
+
+## Testing Variable Types
+
+boulder_precip_in = 28
+
+boulder_precip_in
+
+type(boulder_precip_in)
+
+
+boulder_precip_in = 20.23
+
+boulder_precip_in
+
+type(boulder_precip_in)
+
+
+city = "Boulder"
+
+city
+
+type(city)
+
+city_state = "Boulder, CO"
+
+city_state
+
+city_description = "Boulder, CO is the home of the University of Colorado, Boulder campus."
+
+city_description
+
+# %%
+
+## Interactive Activity
+
+a = 75
+b = float(a)
+print(a, b)
+
+type(b)
+
+# %%
+
+## Interactive Activity - Create Some Variables
+
+precip_float = 46.23
+precip_int = int(precip_float)
+print(precip_float,precip_int)
+location = "New York City"
+
+# %%
+
+## Strings With Numbers in Them
+
+city_precip = "10.68"
+
+type(city_precip)
+
+city_precip * 2
+
+
+
+word_string_test = "word"
+
+word_string_test * 3
+
+# %%
+
+a = 5
+b = 3
+c = a/b
+
+type(a)
+type(b)
+type(c)
+
+
+# %%
