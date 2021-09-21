b42b8f2814156e0dc90d7dc9193ba8b221ed7701
Connal Boyd <cmboyd@email.arizona.edu>
2021-09-21 10:16:42 -0700

    Update numpy test code with class notes

--- working_drafts/Training_codes/test_code_7_numpy.py
+++ working_drafts/Training_codes/test_code_7_numpy.py
@@ -18,3 +18,19 @@ precip_2002_2013 = np.array([
 print(precip_2002_2013)
 
 # %%
+
+test_array = np.random.rand(6,12)
+
+print(test_array)
+
+
+np.round(np.mean(test_array),3)
+
+np.std(test_array)
+
+
+np.mean(test_array[:,2])
+
+
+np.round("Row Averages =", np.mean(test_array, axis =1),2)
+np.round("Column Averages =", np.mean(test_array, axis =0),2)
