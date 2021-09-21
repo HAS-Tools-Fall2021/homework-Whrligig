8fe0789554065d26a4592f135c265633907de200
Connal Boyd <cmboyd@email.arizona.edu>
2021-09-20 19:48:49 -0700

    Created numpy training code

--- working_drafts/Training_codes/test_code_7_numpy.py
+++ working_drafts/Training_codes/test_code_7_numpy.py
@@ -1,1 +1,20 @@
 # %%
+# Numpy Array Training Code
+    # 1-D Arrays
+
+import numpy as np
+
+avg_monthly_precip = np.array([0.70, 0.75, 1.85])
+print(avg_monthly_precip)
+
+# %%
+# 2-D Arrays
+
+precip_2002_2013 = np.array([
+    [1.07, 0.44, 1.50],
+    [0.27, 1.13, 1.72]
+])
+
+print(precip_2002_2013)
+
+# %%
