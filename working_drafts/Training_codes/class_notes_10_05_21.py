b48f5256a07ac01553482c2640153eec8e1fa355
Connal Boyd <cmboyd@email.arizona.edu>
2021-10-05 09:37:42 -0700

    Homework 6 Resubmit, moved plots into correct folder, 10/05 class notes

---
+++ working_drafts/Training_codes/class_notes_10_05_21.py
@@ -0,0 +1,50 @@
+# %%
+
+import os
+import numpy as np
+import pandas as pd
+
+filename = 'streamflow_week2.txt'
+filepath = os.path.join('../data', filename)
+data = pd.read_table(filepath, sep='\t', skiprows=30,
+                     names=['agency_cd', 'site_no',
+                            'datetime', 'flow', 'code'],
+                     parse_dates=['datetime']
+                     )
+
+# Expand the dates to year month day
+data['year'] = pd.DatetimeIndex(data['datetime']).year
+data['month'] = pd.DatetimeIndex(data['datetime']).month
+data['day'] = pd.DatetimeIndex(data['datetime']).day
+data['dayofweek'] = pd.DatetimeIndex(data['datetime']).dayofweek
+
+oct_avgs = np.zeros[31]
+
+for i in range(1,32):
+    day = i + 1
+    data_loop = data[(data['day']==day) & (data['month']==10)]
+    oct_avgs[i] = np.mean(data_loop['flow'])
+
+
+# Class Solution
+oct_mean = np.zeros(31)
+for d in range(31):
+    daytemp = d+1
+    tempdata = data[(data['month']==10) & (data['day'] == daytemp)]
+    oct_mean[d] = np.mean(tempdata['flow'])
+    print('Iteration', d, 'Day=', daytemp, 'Flow=', oct_mean[d])
+
+
+
+def day_mean(month, days_in_month, data):
+    month_mean = np.zeros(days_in_month)
+    for d in range(days_in_month):
+        daytemp = d+1
+        tempdata = data[(data['month']==month) & (data['day'] == daytemp)]
+        month_mean[d] = np.mean(tempdata['flow'])
+        #print('Iteration', d, 'Day=', daytemp, 'Flow=', oct_mean[d])
+    
+    return month_mean
+
+
+day_mean(1, 28, data)
