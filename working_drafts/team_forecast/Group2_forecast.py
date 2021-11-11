984c488dcf6f204188e6d95f553ad290b73fe6d2
Andrew Hoopes <cahoopes@email.arizona.edu>
2021-11-11 14:14:07 -0700

    Web link for streamflow

--- working_drafts/team_forecast/Group2_forecast.py
+++ working_drafts/team_forecast/Group2_forecast.py
@@ -1,3 +1,8 @@
+# @Date:   2021-11-11T14:10:37-07:00
+# @Last modified time: 2021-11-11T14:13:40-07:00
+
+
+
 # %%
 
 import os
@@ -12,22 +17,16 @@ import matplotlib.pyplot as plt
 # Graphs
 # Forecast Numbers
 
-# Setting the file name and path to where we have stored the data.
-filename = 'streamflow_week10.txt'
-filepath = os.path.join('..', 'data', filename)
-print(os.getcwd())
-print(filepath)
+flow_url = "https://waterdata.usgs.gov/nwis/dv?cb_00060=on&format=rdb" \
+           "&site_no=09506000&referred_module=sw" \
+           "&period=&begin_date=1989-01-01&end_date=2021-11-06"
+flow_data = pd.read_table(flow_url, sep='\t', skiprows=30,
+                          names=['agency_cd', 'site_no', 'datetime', 'flow',
+                                 'code'], parse_dates=['datetime'])
 
-# Filepath for forecast submission folder.
-# filename = 'streamflow_week10.txt'
-# filepath = os.path.join('..', '..', 'working_drafts', 'data', filename)
-# print(os.getcwd())
-# print(filepath)
-
-# %%
-# Read the data into a pandas dataframe.
-data = pd.read_table(filepath, sep='\t', skiprows=30,
-                     names=['agency_cd', 'site_no', 'datetime',
-                            'flow', 'code'],
-                     parse_dates=['datetime'], index_col=['datetime']
-                     )
+# Expand the dates to year month day, set index as base datetime
+flow_data['year'] = pd.DatetimeIndex(flow_data['datetime']).year
+flow_data['month'] = pd.DatetimeIndex(flow_data['datetime']).month
+flow_data['day'] = pd.DatetimeIndex(flow_data['datetime']).day
+flow_data['dayofweek'] = pd.DatetimeIndex(flow_data['datetime']).dayofweek
+flow_data = flow_data.set_index('datetime')
