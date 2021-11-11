87b3f7a750e4c9aa012ca08aebc0742b1c018e3d
Connal Boyd <cmboyd@email.arizona.edu>
2021-11-11 12:58:19 -0800

    Added file path for USGS streamflow data to py file, added info to md

--- working_drafts/team_forecast/Group2_forecast.py
+++ working_drafts/team_forecast/Group2_forecast.py
@@ -11,3 +11,23 @@ import matplotlib.pyplot as plt
 # Map
 # Graphs
 # Forecast Numbers
+
+# Setting the file name and path to where we have stored the data.
+filename = 'streamflow_week10.txt'
+filepath = os.path.join('..', 'data', filename)
+print(os.getcwd())
+print(filepath)
+
+# Filepath for forecast submission folder.
+# filename = 'streamflow_week10.txt'
+# filepath = os.path.join('..', '..', 'working_drafts', 'data', filename)
+# print(os.getcwd())
+# print(filepath)
+
+# %%
+# Read the data into a pandas dataframe.
+data = pd.read_table(filepath, sep='\t', skiprows=30,
+                     names=['agency_cd', 'site_no', 'datetime',
+                            'flow', 'code'],
+                     parse_dates=['datetime'], index_col=['datetime']
+                     )
---
+++ working_drafts/team_forecast/ReadMe.md
@@ -0,0 +1,8 @@
+## Group 2 -- Connal Boyd, Andrew Hoopes, Stephanie Serrano, Zhang Xingyu
+
+1 Wk Forecast:/
+2 Wk Forecast:
+
+3.) A brief summary of your collaboration. What did each team member bring to the table, who did what and how did you decide how to combine things?
+
+4.) A summary of your forecast. This should be written as a narrative summary without any blocks of code. It should summarize the inputs and approach used and must include at least 1 map and 1 graph. Only include graphs that you talk about in your narrative.
